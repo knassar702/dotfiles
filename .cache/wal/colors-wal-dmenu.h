19e153eb26e8b46b6f384b307529e8194294c806
Khaled Nassar <knassar702@gmail.com>
2021-08-28 01:22:00 +0200

    remove files

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#dfe0df", "#0f1213" },
-	[SchemeSel] = { "#dfe0df", "#4B4B49" },
-	[SchemeOut] = { "#dfe0df", "#C3C0BC" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0f1213";
-static const char* selfgcolor   = "#dfe0df";
-static const char* normbgcolor  = "#6A6660";
-static const char* normfgcolor  = "#dfe0df";
-static const char* urgbgcolor   = "#4B4B49";
-static const char* urgfgcolor   = "#dfe0df";
